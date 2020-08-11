3dbb9b033ed1bea6b52b747654c1e9f6281e5976
Anderson José Guimarães Araujo <anaraujo@funcaoinformatica.com.br>
2020-08-11 17:13:52 -0300

    Aula 1 - primeira parte

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Landing from './pages/Landing'
+
+import './assets/styles/global.css'
+
+//JSX - JavaScript + XML (Trabalhar com sintaxe de html dentro do javascript)
+function App() {
+  return (
+    <Landing />
+  );
+}
+
+export default App;
